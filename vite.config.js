ec45137bb131eae1298f1e97a50ef7028d228382
Ismail Ceylan <ismaillceylan@gmail.com>
2024-07-10 11:46:21 +0300

    build: add vite

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+// vite.config.js
+import { resolve } from "path"
+import { defineConfig } from "vite"
+
+export default defineConfig(
+{
+	build:
+	{
+		lib:
+		{
+			// Could also be a dictionary or array of multiple entry points
+			entry: resolve( __dirname, "index.js" ),
+			name: "local-average",
+			// the proper extensions will be added
+			fileName: "local-average",
+		},
+		rollupOptions:
+		{
+			// make sure to externalize deps that shouldn't be bundled
+			// into your library
+			external: [],
+			output:
+			{
+				// Provide global variables to use in the UMD build
+				// for externalized deps
+				globals:
+				{
+					// vue: "Vue",
+				},
+			},
+		},
+	},
+});
